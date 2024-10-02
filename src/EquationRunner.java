6f71018d9f4e5d36dd55ab3dd342465f522b3fe2
BT_2S14_09 <email@email.com>
2024-10-02 10:42:53 -0400

    New project created Added instance variables and methods

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class EquationRunner {
+    LinearEquation linearEquation1 = new LinearEquation(1,2,3,4);
 }
 
 
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class LinearEquation {
     private int x1;
     private int x2;
@@ -5,4 +9,51 @@ public class LinearEquation {
     private int y2;
     private boolean noSlope;
     private String equation;
-}
+
+
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public String firstPair;
+
+    {
+        firstPair = x1 + "," + y1;
+
+    }
+
+    public String secondPair;
+
+    {
+        secondPair = x2 + "," + y2;
+
+    }
+
+    public double returnSlope;
+    {
+        returnSlope = (y2-y1)/(x2-x1);
+
+    }
+
+    public double calculateYIntercept;
+    {
+        calculateYIntercept = y1 - (returnSlope-x1);
+    }
+
+    public double calculateDistance;
+    {
+       calculateDistance = Math.sqrt(Math.pow(x2-x1,2))+ Math.pow(y2-y1,2);
+    }
+
+
+
+
+    }
+
+
+
+
