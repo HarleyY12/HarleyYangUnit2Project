e73a2a34abde8fc9a7c829c582eb7e578e56a4dd
HarleyY12 <hyang2496@gmail.com>
2024-10-03 11:46:21 -0400

    Update LinearEquation.java
    Got rid of some redundant instance variables and added ways to convert y- intercepts to fractions and whole numbers

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -7,8 +7,6 @@ public class LinearEquation {
     private double yIntercept;
     private double distance;
     private boolean noSlope;
-    private String equation;
-
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
@@ -22,41 +20,74 @@ public class LinearEquation {
             noSlope = false;
             slope = calculateSlope();
             yIntercept = calculateYIntercept();
-
-
         }
         distance = calculateDistance();
-
-
     }
 
-    private String firstPair(){
-
-        return ( x1 + "," + y1);
-    }
-
-
-    private String secondPair() {
-        return ( x2 + ", " + y2 );
-    }
-
-    
     private double calculateSlope() {
         return (double) (y2 - y1) / (x2 - x1);
-
     }
 
     public double calculateYIntercept() {
-        return  y1 - ( slope * x1 );
+        return y1 - slope * x1;
     }
 
     public double calculateDistance() {
-        return  Math.sqrt(Math.pow( x2 - x1, 2 )) + Math.pow( y2 - y1, 2 );
+        return Math.sqrt(Math.pow(x2 - x1, 2)) + Math.pow(y2 - y1, 2);
     }
 
+    public double getSlope() {
+        return slope;
+    }
 
+    public double getYIntercept() {
+        return yIntercept;
+    }
+    public double getDistance() {
+        return distance;
+    }
+    public String slopeAsFraction() {
+        int numerator = y2 - y1;
+        int denominator = x2 - x1;
+        if (numerator % denominator == 0) {
+            return Integer.toString(numerator / denominator);
+        } else {
+            return numerator + "/" + denominator;
+        }
+    }
+    public String getEquation() {
+        if (noSlope) {
+            return "There is no slope and no linear equation";
+        }
+        String slopeString = slopeAsFraction();
 
+        String interceptString;
+        if (yIntercept == 0) {
+            interceptString = "";
+        } else if (yIntercept > 0) {
+            interceptString = " + " + String.format("%.2f", yIntercept);
+        } else {
+            interceptString = " - " + String.format("%.2f", Math.abs(yIntercept));
+        }
+        return "y = " + slopeString + "x" + interceptString;
+    }
+
+    public String toString() {
+        String result = "First point: (" + x1 + ", " + y1 + ")\n" +
+                "Second point: (" + x2 + ", " + y2 + ")\n" +
+                "Distance between the points: " + String.format("%.2f", distance) + "\n";
+        if (noSlope) {
+            result += "The line is vertical; slope is undefined.\n";
+        } else {
+            result += "Equation of the line: " + getEquation() + "\n" +
+                    "Slope: " + slopeAsFraction() + "\n" +
+                    "Y-intercept: " + String.format("%.2f", yIntercept) + "\n";
+        }
+        return result;
+    }
 }
+
+
 
 
 
