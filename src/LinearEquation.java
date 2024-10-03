b74444a7a2dd45a600d1adb731f10e3db6914236
HarleyY12 <hyang2496@gmail.com>
2024-10-02 20:50:44 -0400

    Update LinearEquation.java

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,16 +1,13 @@
-
-
-
-
 public class LinearEquation {
     private int x1;
     private int x2;
     private int y1;
     private int y2;
+    private double slope;
+    private double yIntercept;
+    private double distance;
     private boolean noSlope;
     private String equation;
-    
-
 
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
@@ -18,42 +15,48 @@ public class LinearEquation {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-    }
 
-    public String firstPair;
-
-    {
-        firstPair = x1 + "," + y1;
-
-    }
-
-    public String secondPair;
-
-    {
-        secondPair = x2 + "," + y2;
-        
-    }
-
-    public double returnSlope;
-    {
-        returnSlope = (y2-y1)/(x2-x1);
-
-    }
-
-    public double calculateYIntercept;
-    {
-        calculateYIntercept = y1 - (returnSlope-x1);
-    }
-
-    public double calculateDistance;
-    {
-       calculateDistance = Math.sqrt(Math.pow(x2-x1,2))+ Math.pow(y2-y1,2);
-    }
+        if (x1 == x2) {
+            noSlope = true;
+        } else {
+            noSlope = false;
+            slope = calculateSlope();
+            yIntercept = calculateYIntercept();
 
 
+        }
+        distance = calculateDistance();
 
 
     }
+
+    private String firstPair(){
+
+        return ( x1 + "," + y1);
+    }
+
+
+    private String secondPair() {
+        return ( x2 + ", " + y2 );
+    }
+
+    
+    private double calculateSlope() {
+        return (double) (y2 - y1) / (x2 - x1);
+
+    }
+
+    public double calculateYIntercept() {
+        return  y1 - ( slope * x1 );
+    }
+
+    public double calculateDistance() {
+        return  Math.sqrt(Math.pow( x2 - x1, 2 )) + Math.pow( y2 - y1, 2 );
+    }
+
+
+
+}
 
 
 
