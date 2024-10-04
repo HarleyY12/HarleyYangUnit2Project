c5120b9a3f29af48bb1079f2e2da2cc0499a0505
HarleyY12 <hyang2496@gmail.com>
2024-10-04 10:52:15 -0400

    Update EquationRunner.java

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter coordinates for the first point (x1, y1): ");
+        System.out.println("Enter the first coordinate point: ");
         System.out.print("x1: ");
         int x1 = s.nextInt();
         System.out.print("y1: ");
         int y1 = s.nextInt();
-        System.out.println("Enter coordinates for the second point (x2, y2): ");
+        System.out.println("Enter the second coordinate point: ");
         System.out.print("x2: ");
         int x2 = s.nextInt();
         System.out.print("y2: ");
