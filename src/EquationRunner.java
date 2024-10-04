94241797516bdd55cdb76b8112bc1dc18bf7a168
HarleyY12 <hyang2496@gmail.com>
2024-10-04 10:49:18 -0400

    Update EquationRunner.java
    Worked on runner class.Let the user input integers and program outputs the information like distance
    Imported a scanner and introduced new instance of the linear equation class.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
-
 public class EquationRunner {
-    LinearEquation linearEquation1 = new LinearEquation(1,2,3,4);
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter coordinates for the first point (x1, y1): ");
+        System.out.print("x1: ");
+        int x1 = s.nextInt();
+        System.out.print("y1: ");
+        int y1 = s.nextInt();
+        System.out.println("Enter coordinates for the second point (x2, y2): ");
+        System.out.print("x2: ");
+        int x2 = s.nextInt();
+        System.out.print("y2: ");
+        int y2 = s.nextInt();
+        LinearEquation equation = new LinearEquation(x1,y1,x2,y2);
+        System.out.println(equation);
+    }
 }
-
 
 
 
