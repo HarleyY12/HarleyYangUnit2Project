3328eb6a856d59d5343eb996d3bfa54c9ad0a8e5
HarleyY12 <hyang2496@gmail.com>
2024-10-04 19:21:00 -0400

    Update LinearEquation.java
    I added comments to the code for future reference and to help others understand the code better.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,6 +1,6 @@
-
-
+//New Class
 public class LinearEquation {
+    //Instance Variables
     private final int x1;
     private final int x2;
     private final int y1;
@@ -9,11 +9,13 @@ public class LinearEquation {
     private final double distance;
     private final boolean noSlope;
 
+    //Constructor
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        //Check If Line is Vertical
         if (x1 == x2 ) {
             noSlope = true;
         } else {
@@ -22,15 +24,19 @@ public class LinearEquation {
         }
         distance = calculateDistance();
     }
+    //Method to Calculate Slope
     private double calculateSlope() {
         return (double) (y2 - y1) / (x2 - x1);
     }
+    //Method to Calculate Y-Intercept
     public double calculateYIntercept() {
         return y1 - calculateSlope() * x1;
     }
+    //Method to Calculate the Distance Between 2 Points
     public double calculateDistance() {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
+    //Method to Return the Slope as a Fraction String
     public String slopeAsFraction() {
         int numerator = y2 - y1;
         int denominator = x2 - x1;
@@ -50,24 +56,28 @@ public class LinearEquation {
             return numerator + "/" + denominator;
         }
     }
+    //Method to Return the Linear Equation as a String
     public String getEquation() {
         String slopeString = slopeAsFraction();
         String interceptString;
+        //Initializes Rewrites Slope as "" if 1 or "-" if -1
         if (calculateSlope() ==1){
             slopeString = "";
         }
         if (calculateSlope() == -1){
             slopeString = "-";
         }
+        //Format the Y-Intercept String
         if (yIntercept == 0) {
             interceptString = "";
-        } else if (yIntercept > 0) {
+        }else if (yIntercept > 0) {
             interceptString = " + " + String.format("%.2f", yIntercept);
-        } else {
+        }else{
             interceptString = " - " + String.format("%.2f", Math.abs(yIntercept));
         }
         return "y = " + slopeString + "x" + interceptString;
     }
+    //Returns Details of Linear Equation
     public String toString() {
         if (noSlope) {
             return "First point: (" + x1 + "," + y1 + ")\n" +
@@ -84,7 +94,12 @@ public class LinearEquation {
                     "Y-intercept: " + String.format("%.2f",yIntercept);
         }
     }
+    //Method to Solve for the Y-Value of a 3rd Coordinate Point Given x3
     public double solveForY3(double x3) {
         return calculateSlope() * x3 + yIntercept;
     }
 }
+
+
+
+
